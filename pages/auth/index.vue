55a738cac969c27fc4eaeba5b10a490890083975
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2022-12-04 07:55:47 -0300

    useFetch

---
+++ pages/auth/index.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="form">
+        <div class="input">
+            <input type="text" v-model="email" placeholder="Seu melhor email...">
+        </div>
+        <div class="input">
+            <input type="password" v-model="password" placeholder="Sua Senha...">
+        </div>
+
+        <button @click="onSubmit" class="entry">Entrar</button>
+
+    </div>
+
+
+</template>
+<script>
+definePageMeta({
+    layout: "auth"
+})
+
+import axios from "axios"
+export default {
+    data() {
+        return {
+            email: "",
+            password: ""
+        }
+    },
+    methods: {
+        onSubmit() {
+            axios.post("http://localhost:3001/login",
+                { email: this.email, password: this.password })
+                .then(response => {
+                    console.log(response.data.data)
+                })
+                .catch(error => {
+                    console.error(error.response.data.errorPassword);
+                })
+            // await useFetch("http://localhost:3001/login", {
+            //     method: "post",
+            //     body: { email: this.email, password: this.password }
+            // }).then(response => {
+
+            // console.log(response.data)
+            // })
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
